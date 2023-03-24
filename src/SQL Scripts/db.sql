82ef3d9e806bb4f24bb050c8be0fa7b2fe723496
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-24 15:24:59 +0300

    first commit

---
+++ src/SQL Scripts/db.sql
@@ -0,0 +1,6 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [Age]
+      ,[Gender]
+      ,[Period]
+      ,[Unemployed]
+  FROM [Unemployment Data].[dbo].[unemployem
